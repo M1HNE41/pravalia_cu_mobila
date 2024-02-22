5ea0bd0d60438ed04aa29b1a9e6b4a6a0bd7549c
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-02-22 02:27:48 +0200

    Update struct.php

--- api/struct.php
+++ api/struct.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-include 'style.css';
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,6 +16,7 @@ include 'style.css';
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.0.1/tailwind.min.css'>
+<link rel='stylesheet' href='style.css'>
 </head>
 
 
