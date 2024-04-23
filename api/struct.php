cd57ad8fc6325ddebcd4d2e2d8b7dfd572eb8172
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-23 17:51:33 +0300

    Update struct.php

--- api/struct.php
+++ api/struct.php
@@ -321,7 +321,7 @@ function ipToConcatenatedString($ip) {
     echo "
     <div class='card card-item lg:w display: inline-block'>
         <div class='bg-white shadow-xl rounded-lg overflow-hidden'>
-            <div class='bg-cover bg-center h-56 p-4' style='width: 100%; height: 200px; object-fit: cover; background-image: url($imgUrl)'>
+            <div class='aspect-square bg-cover bg-center h-56 p-4' style=' background-image: url($imgUrl)'>
             </div>
             <div class='p-4'>
                 <p class='uppercase tracking-wide text-sm font-bold text-gray-700'>$title</p>
