504595e6954e27af0cbc52402053d6de09deddbb
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-24 22:11:29 +0300

    Update baie.php

--- api/baie.php
+++ api/baie.php
@@ -59,7 +59,7 @@ Magazinul nostru va oferă o gamă extinsă de mobilă pentru dormitor, paturi c
 
 <?php
 echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
-$productNumber=1;
+$productNumber=0;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
         echo '<div style ="width: 30%; margin-bottom: 20px">';
@@ -80,7 +80,6 @@ echo '</div>'; // end the row
 
 <?php
 echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
-$productNumber=7;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
         echo '<div style ="width: 30%; margin-bottom: 20px">';
@@ -105,7 +104,6 @@ echo '</div>'; // end the row
 
 <?php
 echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
-$productNumber=13;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
         echo '<div style ="width: 30%; margin-bottom: 20px">';
@@ -127,7 +125,6 @@ echo '</div>'; // end the row
 </center>
 <?php
 echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
-$productNumber=19;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
         echo '<div style ="width: 30%; margin-bottom: 20px">';
@@ -150,7 +147,6 @@ echo '</div>'; // end the row
 
 <?php
 echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
-$productNumber=25;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
         echo '<div style ="width: 30%; margin-bottom: 20px">';
