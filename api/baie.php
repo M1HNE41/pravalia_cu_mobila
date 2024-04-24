1a9015ee1ba5641c72f905a09989d7e2f41b9e40
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-24 21:48:57 +0300

    Update baie.php

--- api/baie.php
+++ api/baie.php
@@ -79,10 +79,17 @@ echo '</div>'; // end the row
 </center>
 
 <?php
-echo '<div class="flex flex-wrap justify-center">'; // start a new row
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+$productNumber=1;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
-        generateCard("https://jysk.ro/sites/jysk.ro/files/category_top_banner/S367090-JEGIND-JONSTRUP-3670143-HALMSTAD.jpg", "Cuvertura", "920", "cuvertura moale", "100x100cm", "textil"); // replace with actual data
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
     }
     echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
 }
@@ -97,10 +104,17 @@ echo '</div>'; // end the row
 <hr color="black"  width="auto" >
 
 <?php
-echo '<div class="flex flex-wrap justify-center">'; // start a new row
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+$productNumber=1;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
-        generateCard("https://jysk.ro/sites/jysk.ro/files/category_top_banner/S367090-JEGIND-JONSTRUP-3670143-HALMSTAD.jpg", "Cuvertura", "920", "cuvertura moale", "100x100cm", "textil"); // replace with actual data
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
     }
     echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
 }
@@ -112,10 +126,17 @@ echo '</div>'; // end the row
 	</bold>	
 </center>
 <?php
-echo '<div class="flex flex-wrap justify-center">'; // start a new row
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+$productNumber=1;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
-        generateCard("https://jysk.ro/sites/jysk.ro/files/category_top_banner/S367090-JEGIND-JONSTRUP-3670143-HALMSTAD.jpg", "Cuvertura", "920", "cuvertura moale", "100x100cm", "textil"); // replace with actual data
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
     }
     echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
 }
@@ -128,10 +149,17 @@ echo '</div>'; // end the row
 </center>
 
 <?php
-echo '<div class="flex flex-wrap justify-center">'; // start a new row
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+$productNumber=1;
 for ($i = 0; $i < 2; $i++) { // generate 2 rows
     for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
-        generateCard("https://jysk.ro/sites/jysk.ro/files/category_top_banner/S367090-JEGIND-JONSTRUP-3670143-HALMSTAD.jpg", "Cuvertura", "920", "cuvertura moale", "100x100cm", "textil"); // replace with actual data
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
     }
     echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
 }
