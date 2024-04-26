e7633d7bfaec3a06b6c731f0f0e99a7c3ab4f82e
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-26 16:47:26 +0300

    Update accesorii bucatarie.php

--- api/accesorii bucatarie.php
+++ api/accesorii bucatarie.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php include 'struct.php'; ?>
+<?php include 'struct.php';
+$dbconn = pg_connect("host=aws-0-eu-central-1.pooler.supabase.com port=5432 dbname=postgres user=postgres.piasuguypoushrpezbmu password=~2T-Ee7t#~PLPa6")
+or die('Could not connect: ' . pg_last_error());
+?>
 <div style="clear:both;"> </div>
 <center>
 	<h1>
-	<b>Bucatarie</b>
+	<b>Dormitor</b>
 </h1>
 </center>
 
@@ -14,22 +17,22 @@ Magazinul nostru va oferă o gamă extinsă de mobilă pentru dormitor, paturi c
 </p>
 <hr color="grey"  width="auto" >
 <div class="gallery">
-  <img class="card-img-top" src="Poze produse\Dormitor1.jpeg">
+  <img class="card-img-top" src="Poze produse\Dormitor1.jpg">
   
 </div>
 
 <div class="gallery">
-  <img class="card-img-top" src="Poze produse\Dormitor2.jpeg">
+  <img class="card-img-top" src="Poze produse\Dormitor2.jpg">
   
 </div>
 
 <div class="gallery">
-  <img class="card-img-top" src="Poze produse\Dormitor3.jpeg">
+  <img class="card-img-top" src="Poze produse\Dormitor3.jpg">
   
 </div>
 
 <div class="gallery">
-  <img class="card-img-top" src="Poze produse\Dormitor4.jpeg">
+  <img class="card-img-top" src="Poze produse\Dormitor4.jpg">
   
 </div>
 <br>
@@ -50,329 +53,93 @@ Magazinul nostru va oferă o gamă extinsă de mobilă pentru dormitor, paturi c
 <hr color="black"  width="1900px" >
 <br>
 <br>
-
-<div class="vertical-menu">
-  <a href="#bufete/dulapuri" >Bufete/Dulapuri</a>
-  <hr color="#DCD9D8"  width="100%" >
-  <a href="#mese dining">Mese dining</a>
-  <hr color="#DCD9D8"  width="100%" >
-  <a href="#scaune dining">Scaune dining</a>
-  <hr color="#DCD9D8"  width="100%" >
-  <a href="#rafturi">Rafturi</a>
-  <hr color="#DCD9D8"  width="100%" >
-  <a href="#seturi dining" >Seturi dining</a>
-</div>
 <center>
-<h3 id="bufete/dulapuri">DULAPURI, BUFETE SI VITRINE</h3>
+<h3 >PATURI ȘI CADRE DE PAT REZISTENTE</h3>
 </center>
-<hr color="black"  width="absolute" >
 
-<p>Un dulap inteligent de bucatarie sau o vitrina moderna este o modalitate clasica de a crea mai mult spatiu de depozitare pentru accesorii sau obiecte esentiale precum tacamuri, vesela, pahare, ustensile, carti si fotografii. Alege un bufet de bucatarie sau living, o vitrina sau un dulap de bucatarie in diferite culori si materiale: lemn natur, alb, negru, metal, nuc, frasin, stejar si va fi mai usor sa iti organizezi lucrurile.
-</p>
-  <div class="card-row">
-  <div class="card">
-    <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\bufet1.jpeg" style="width:100%">
-    <h3 style="text-align: center;">Bufet 3 usi alb</h3>
-    <p class="price">1999.00 RON</p>
-    <p>Lemn masiv. Sertare cu sistem de inchidere lenta. 140x82x42 cm.</p>
-    
-  </div>
-
-      <div class="card">
-        <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap1.jpeg" style="width:100% ">
-        <h3 style="text-align: center;">Dulap superior 2 usi alb</h3>
-        <p class="price">1399.00 RON</p>
-        <p>Furnir decorativ, MDF si sticla securizata. 132x111x36 cm.</p>
-        
-      </div>
-
-
-      <div class="card">
-        <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\bufet2.jpeg" style="width:100%">
-        <h3 style="text-align: center;">Bufet + dulap superior gri</h3>
-        <p class="price">3098.00 RON</p>
-        <p>Bufet: Furnir decorativ si MDF. 132x88x43 cm. Dulap superior: Furnir decorativ, MDF si sticla securizata. 132x111x36 cm.</p>
-        
-      </div> 
-
-      <div class="card">
-        <img  class="card-img-top" src="Poze produse\Accesorii Bucatarie\vitrina1.jpeg" style="width:100%">
-        <h3 style="text-align: center;">Vitrina 2 usi alb</h3>
-        <p class="price">1499.00 RON</p>
-        <p>Lemn masiv, furnir decorativ si sticla securizata. 80x150x40 cm.</p>
-        
-      </div>
-      </div>
-
-<div class="card">
-  <img class="card-img-top"  src="Poze produse\Accesorii Bucatarie\bufet3.jpeg" style="width:100%">
-  <h3 style="text-align: center;">Bufet 3 usi alb</h3>
-  <p class="price">1999.00 RON</p>
-  <p>Lemn masiv. Sertare cu sistem de inchidere lenta. 140x82x42 cm.                                                                                               
-
-  </p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\vitrina2.jpeg" style="width:100%">
-  <h3 style="text-align: center;">Vitrina 2 usi stejar</h3>
-  <p class="price">1650.00 RON</p>
-  <p>Lemn masiv de stejar, furnir de stejar si sticla securizata. 80x126x35 cm                                                                                              
-
-  </p>
-</div>   
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cadru140x200.jpeg" style="width:100%">
-  <h3 style="text-align: center;">Somiera pat 140x200 cm</h3>
-  <p class="price">450.00 RON</p>
-  <p>Somiera cu cadru metalic si lamele elastice, 140 x 200 cm </p>
-</div>
-
-<div class="card">
-  <img class="card-img-top"  src="Poze produse\Accesorii Bucatarie\cadru140x200.jpeg" style="width:100%">
-  <h3 style="text-align: center;">Somiera pat 160x200 cm</h3>
-  <p class="price">450.00 RON</p>
-  <p>Somiera cu cadru metalic si lamele elastice, 160 x 200 cm </p>    
-</div>
-
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+$productNumber=24;
+for ($i = 0; $i < 2; $i++) { // generate 2 rows
+    for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
+    }
+    echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
+}
+echo '</div>'; // end the row
+?>
 <center>
-<h3 id="cuverturi">Cuverturi</h3>
+<h3 >Cuverturi</h3>
 </center>
-<hr color="black"  width="auto" >
 
-<p>Cuverturile fac patul și întregul dormitor să arate bine. Cel mai evident beneficiu al acestora este că înfrumusețează aspectul dormitorului, făcându-l să pară mai ordonat decât dacă ar fi avut doar lenjeria de pat la vedere. Unii oameni compară un pat nefăcut cu vasele nespălate lăsate în chiuvetă peste noapte.
-Cuvertura asigură un aspect consistent pentru dormitorul tău de care te poți bucura în fiecare zi. Poți schimba lenjeria de pat ori de câte ori dorești și poți alege dintre diferite culori și modele, însă o cuvertură de pat va asigura mereu același aspect îngrijit.</p>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv1.jpeg" style="width:92%">
-  <h4 style="text-align: center;">Cuvertură ROSENTRE 160x220 albastră</h4>
-  <p class="price">450.00 RON</p>
+<?php
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+for ($i = 0; $i < 2; $i++) { // generate 2 rows
+    for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
+    }
+    echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
+}
+echo '</div>'; // end the row
+?>
 </div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv2.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură STORRAPP 160x220 gri</h4>
-  <p class="price">450.00 RON</p>
- 
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv3.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură HENGEVING 160x220 gri</h4>
-  <p class="price">450.00 RON</p>
- 
-</div>
-
-
-<div class="card">
-  <img class="card-img-top" class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv4.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură ROSENTRE 220x240 gri</h4>
-  <p class="price">450.00 RON</p>
-
-</div>
-
-<div class="card">
-  <img  class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv5.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură ROSENTRE 220x240 gri</h4>
-  <p class="price">450.00 RON</p>
-
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv6.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură ROSENTRE 220x240 gri</h4>
-  <p class="price">450.00 RON</p>
-
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv7.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură ROSENTRE 220x240 gri</h4>
-  <p class="price">450.00 RON</p>
-
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cuv8.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cuvertură ROSENTRE 220x240 gri</h4>
-  <p class="price">450.00 RON</p>
-
-</div>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <center>
 	<bold>
-	<h3 id="cearșafuri"> Cearșafuri </h3>
+	<h3> Cearșafuri </h3>
 	<bold>
 </center>
 <hr color="black"  width="auto" >
-<p>Cearșafurile sunt o soluție simplă de a oferi dormitorului tău un aspect nou cu un buget mic. Disponibile într-o varietate de tipuri de fire, niveluri de calitate și culori, este o idee bună să te familiarizezi cu numeroasele opțiuni de cearșafuri pe care ți le oferim. Alege un cearșaf confortabil cu elastic, un cearsaf plic sau unul cu strat absorbant, deoarece știm cu toții cât de bine este să te simți într-un pat cu cearsafuri curate. La noi vei găsi o gamă variată de cearșafuri de cea mai bună calitate, în mărimi, dimensiuni și materiale variate. Alege un cearșaf de pat de calitate și vei dormi mai bine și mai sănătos.</p>
 
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf1.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf elastic KRONBORG</h4>
-  <p class="price">25 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf2.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf impermeabil</h4>
-  <p class="price">50 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf3.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf standard</h4>
-  <p class="price">17.50 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf4.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf Micro</h4>
-  <p class="price">20 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf5.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf flanel</h4>
-  <p class="price">35 ron</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf6.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf flanel</h4>
-  <p class="price">35 ron</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf7.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf marou</h4>
-  <p class="price">35 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\cearceaf8.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Cearsaf gri deschis</h4>
-  <p class="price">35 RON</p>
-</div>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+for ($i = 0; $i < 2; $i++) { // generate 2 rows
+    for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
+    }
+    echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
+}
+echo '</div>'; // end the row
+?>
 <center>
 	<bold>
-		<h3 id="dulap"> Dulapuri din lemn masiv </h3>
+		<h3> Dulapuri din lemn masiv </h3>
 	</bold>	
 </center>
-<hr color="black"  width="auto" >
-<p>Dulapul este o piesă esențială de mobilier în dormitorul tău. Chiar dacă îți place să aduni noi piese de îmbrăcăminte în fiecare sezon sau preferi un stil minimalist, bazat doar pe câteva piese de bază. Alege dintr-o gamă variată de dulapuri, dressinguri cu uși glisante și șifoniere încăpătoare și de calitate, cu dimensiuni, finisaje, și culori diferite. Astfel, vei fi sigur că spațiul tău de depozitare nu este doar funcțional, dar și aspectuos și se încadrează în decorul dormitorului. Un dulap de haine poate avea oglinzi, bare de haine, umerase, sine, dulapioare interioare, mai multe uși sau sertare pentru ca tu să ai suficient spațiu pentru toate tipurile de haine și accesorii.</p>
+<?php
+echo '<div class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">'; // start a new row
+for ($i = 0; $i < 2; $i++) { // generate 2 rows
+    for ($j = 0; $j < 3; $j++) { // generate 3 cards in each row
+        echo '<div style ="width: 30%; margin-bottom: 20px">';
+        $productNumber++;
+        $sql = "SELECT * FROM products WHERE id = '$productNumber'";
+        $result = pg_query($dbconn, $sql);
+        $product = pg_fetch_assoc($result);
+        generateCard($product['image_url'], $product['name'], $product['price'], $product['description'], $product['dimensions']);
+        echo'</div>';
+    }
+    echo '<div style="flex-basis: 100%; height: 0;"></div>'; // this will force wrapping to the next line
+}
+echo '</div>'; // end the row
+?>
 
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap1.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap VINDERUP combi stejar</h4>
-  <p class="price">500 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap2.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap VINDERUP combi</h4>
-  <p class="price">550 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap3.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap VINDERUP combi stejar</h4>
-  <p class="price">650 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap4.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap VINDERUP combi stejar</h4>
-  <p class="price">1799 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap5.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap MANDERUP stejar sălbatic</h4>
-  <p class="price">650 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap6.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap MANDERUP stejar sălbatic</h4>
-  <p class="price">1850 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap7.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap VEDDE stejar sălbatic</h4>
-  <p class="price">1999 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\dulap8.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Dulap VEDDE stejar sălbatic</h4>
-  <p class="price">2499 RON</p>
-</div>
-
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<center>
-	<bold>
-		<h3 id="noptiera"> Noptiere </h3>
-	</bold>
-</center>
-<hr color="black"  width="auto" >
-<p>Indiferent dacă o folosești ca suport pentru veioza de citit sau ca să îți păstrezi cartea și ochelarii peste noapte, o noptieră este o piesă de mobilier pentru dormitor indispensabilă! Alege dintr-o gamă largă de seturi de dormitoare în care piesele se completează, noptiere albe sau din lemn masiv și dulăpioare pentru a adăuga un plus de estetică designului interior al dormitorului. Vezi gama noastră variată de noptiere pentru dormitor. Alege o noptieră din lemn masiv, cu design scandinav, cu unul sau mai multe sertare.</p>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera1.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptieră MANDERUP 1 sertar stejar sălbat</h4>
-  <p class="price">250 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera2.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptieră KABDRUP 1 sertar alb</h4>
-  <p class="price">110 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera3.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptieră KABDRUP 1 sertar stejar</h4>
-  <p class="price">125 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera4.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptieră LIMFJORDEN 2 sertare stejar</h4>
-  <p class="price">200 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera5.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptiera ASKVOLL 2 sertare stejar alb</h4>
-  <p class="price">149 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera6.png" style="width:100%">
-  <h4 style="text-align: center;">Noptieră CAAF4</h4>
-  <p class="price">553 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera7.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptiera tapitata alba Mito</h4>
-  <p class="price">449 RON</p>
-</div>
-
-<div class="card">
-  <img class="card-img-top" src="Poze produse\Accesorii Bucatarie\noptiera8.jpeg" style="width:100%">
-  <h4 style="text-align: center;">Noptiera Azurro verde catifea</h4>
-  <p class="price">478 RON</p>
-</div>
 
 <?php include 'footer.php'; ?>
 
