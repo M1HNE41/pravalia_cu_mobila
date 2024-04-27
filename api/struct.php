cb32edf5b6ef8c020ff68aa84f820525f4e3c07a
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-27 16:21:06 +0300

    Update struct.php

--- api/struct.php
+++ api/struct.php
@@ -1,20 +1,15 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="ro">
 <head>
   <style>
-        .btn {                                       
-  background-color: DodgerBlue;
-  border: none;
-  color: white;
-  padding: 14px 16px;
-  font-size: 16px;
-  cursor: pointer;
-    }
 body {
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
+  background-color: #ffffff;
   overflow-x: hidden;
+  font-family: 'Poppins', sans-serif;
+  color: #000000;
 }
 
 a {
@@ -22,92 +17,49 @@ a {
   text-decoration: none;
 }
 
+.header {
+            background-color: #ffffff;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 20px;
+        }
+
+        .logo {
+            color: #000000;
+            text-decoration: none;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .menu {
+            display: flex;
+            align-items: center;
+        }
+
+        .menu a {
+            color: #000000;
+            text-decoration: none;
+            margin-left: 20px;
+            font-size: 18px;
+            font-family: 'Poppins', sans-serif; /* Schimbăm fontul butoanelor */
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
 
 
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
 
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 16px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-.topnav input[type=text] {
-  float: right;
-  padding: 6px;
-  border: none;
-  margin-top: 8px;
-  margin-right: 16px;
-  font-size: 14px;
-}
-div.relative {
-  position: relative;
-  left: -30px;
-
-}
-/* The subnavigation menu */
-.subnav {
-  float: left;
-  overflow: hidden;
-}
-
-/* Subnav button */
-.subnav .subnavbtn {
-  font-size: 16px;
-  border: none;
-  outline: none;
-  color: white;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-}
-
-/* Add a red background color to navigation links on hover */
-.navbar a:hover, .subnav:hover .subnavbtn {
-  background-color: grey;
-}
-
-/* Style the subnav content - positioned absolute */
-.subnav-content {
-  display: none;
-  position: absolute;
-  left: 0;
-  background-color: grey;
-  width: 100%;
-  z-index: 1;
-}
-
-/* Style the subnav links */
-.subnav-content a {
-  float: left;
-  color: white;
-  text-decoration: none;
-}
-
-/* Add a grey background color on hover */
-.subnav-content a:hover {
-  background-color: #eee;
-  color: black;
-}
-.subnav:hover .subnav-content {
-  display: block;
-}
 
 .footer {
   padding: 1rem;
@@ -167,14 +119,13 @@ div.relative {
 .vertical-menu2 a:hover {
   background-color: transparent;
 }    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Pravalia cu mobila</title>
-  <div class="relative">
-    <div id="logo">
-    <a href="/home">
-      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9sb2dvLnBuZyIsImlhdCI6MTcwODYzNjAwNCwiZXhwIjozMTU1Mjc3MTAwMDA0fQ.eFwp7a94N-8GmvY0gGljf3OpE5llbVJL6BmgMMPhwFU&t=2024-02-22T21%3A06%3A45.022Z" alt="logo" width="400px" height="95px">
-      </a>
-  </div>
-  </div>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+ 
  
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -186,23 +137,25 @@ div.relative {
 
 
 <body>
-<div class="topnav">
- <div class="subnav">
-   <button class="subnavbtn"><i class="fa fa-bars"> Mobila</i></button>
-    <div class="subnav-content">
-      <a href="/mobila-dormitor">Mobila Dormitor</a>
-      <a href="/mobila-living">Mobila Living</a>
-      <a href="/accesorii-bucatarie">Accesorii bucatarie</a>
-      <a href="/baie">Baie</a>
-    </div>
-  </div>   
-
-  <a class="fa fa-phone"> Contact</a>
-  <a class="fa fa-info-circle"> Informatii</a>
-  <a class="fa fa-legal"> Termeni si conditii</a>
-  <a class="fa fa-wrench"> Servicii</a>
-  <a class="fa fa-copyright"> Scurt istoric</a>
-    <?php
+    <header class="header">
+        <a href="/home" class="logo"><img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9sb2dvLnBuZyIsImlhdCI6MTcwODYzNjAwNCwiZXhwIjozMTU1Mjc3MTAwMDA0fQ.eFwp7a94N-8GmvY0gGljf3OpE5llbVJL6BmgMMPhwFU&t=2024-02-22T21%3A06%3A45.022Z" alt="logo" width="400px" height="95px">
+        </a>
+        <nav class="menu">
+            <div class="dropdown">
+                <a href="#" class="dropdown-btn">Mobila</a>
+                <div class="dropdown-content">
+                    <a href="/mobila-dormitor">Mobila Dormitor</a>
+                    <a href="/mobila-living">Mobila Living</a>
+                    <a href="/accesorii-bucatarie">Accesorii Bucătărie</a>
+                    <a href="/baie">Accesorii Baie</a>
+                </div>
+            </div>
+            <a class="fa fa-phone"> Contact</a>
+  	    <a class="fa fa-info-circle"> Informatii</a>
+  	    <a class="fa fa-legal"> Termeni si conditii</a>
+ 	    <a class="fa fa-wrench"> Servicii</a>
+  	    <a class="fa fa-copyright"> Scurt istoric</a>
+<?php
 $dbconn = pg_connect("host=aws-0-eu-central-1.pooler.supabase.com port=5432 dbname=postgres user=postgres.piasuguypoushrpezbmu password=~2T-Ee7t#~PLPa6")
 or die('Could not connect: ' . pg_last_error());
 function get_client_ip() {
@@ -248,7 +201,6 @@ function ipToConcatenatedString($ip) {
             $username = pg_fetch_assoc($result_user)['username'];
             echo '<a href="#">Welcome, ' . $username . '</a>';
             echo '<form method="post">
-    <!-- ... (your existing HTML code) -->
     <button type="submit" name="logout">Logout</button>
 </form>';
             if (isset($_POST['logout'])) {
@@ -277,31 +229,6 @@ function ipToConcatenatedString($ip) {
     echo '<a class="fa fa-sign-out" href="/signup">Signup</a>';
 }
 ?>
-
-  
-</div>
-<?php
-   function generateCard($imgUrl, $title, $price, $description, $dimensions) {
-    echo "
-    <div class='card card-item lg:w display: inline-block'>
-        <div class='bg-white shadow-xl rounded-lg overflow-hidden'>
-            <div class='bg-cover bg-center h-56 p-4' style='background-image: url($imgUrl)'>
-            </div>
-            <div class='p-4'>
-                <p class='uppercase tracking-wide text-sm font-bold text-gray-700'>$title</p>
-                <p class='text-3xl text-gray-900'>$$price</p>
-                <p class='text-gray-700'>$description</p>
-            </div>
-            <div class='flex p-4 border-t border-gray-300 text-gray-700'>
-                <div class='flex-1 inline-flex items-center'>
-                    <p><span class='text-gray-900 font-bold'>$dimensions</span> Dimensions</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    ";
-}
-    ?>
 </body>
 </html>
 
