8eb29656882f36d64c73e0f2174acabf026a8a2d
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-28 16:25:57 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -9,37 +9,47 @@ include 'struct.php';
 <center>
     <b>CATEGORII</b>
 </center>
+<div id="mz-gallery-container">
 
-<div class="gallery">
-  <a href="/mobila-dormitor">
-  <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Dormitor.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9Eb3JtaXRvci5qcGciLCJpYXQiOjE3MDg2NDA3NzYsImV4cCI6MzE1NTI3NzEwNDc3Nn0.LQao52pUQp4-CPqNf0qt7SsLgQ1zZo8EgIzO0-uMKxQ&t=2024-02-22T22%3A26%3A17.390Z">
-  </a>
-  <div class="desc">Dormitor</div>
-</div>
+  <div id="mz-gallery" class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">
+    <figure style ="width: 30%; margin-bottom: 20px">
+      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Dormitor.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9Eb3JtaXRvci5qcGciLCJpYXQiOjE3MDg2NDA3NzYsImV4cCI6MzE1NTI3NzEwNDc3Nn0.LQao52pUQp4-CPqNf0qt7SsLgQ1zZo8EgIzO0-uMKxQ&t=2024-02-22T22%3A26%3A17.390Z" alt="Statue of Liberty" width="4" height="100">
+      <figcaption>Statue of Liberty</figcaption>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </figure>
 
-<div class="gallery">
-  <a href="/mobila-living">
-  <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwODAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z"></a>
-  <div class="desc">Living</div>
+    <figure style ="width: 30%; margin-bottom: 20px">
+      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwODAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z" alt="Night Sky" width="100" height="700">
+      <figcaption>Night Sky</figcaption>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </figure>
+    <div style="flex-basis: 100%; height: 0;"></div> <!-- this will force wrapping to the next line -->
+    <figure style ="width: 30%; margin-bottom: 20px">
+      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Dormitor.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9Eb3JtaXRvci5qcGciLCJpYXQiOjE3MDg2NDA3NzYsImV4cCI6MzE1NTI3NzEwNDc3Nn0.LQao52pUQp4-CPqNf0qt7SsLgQ1zZo8EgIzO0-uMKxQ&t=2024-02-22T22%3A26%3A17.390Z" alt="Ravine Between Rocks" width="400" height="700">
+      <figcaption>Ravine Between Rocks</figcaption>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </figure>
 
-</div>
+    <figure style ="width: 30%; margin-bottom: 20px">
+      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwODAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z" alt="Ravine Between Rocks" width="400" height="700">
+      <figcaption>Ravine Between Rocks</figcaption>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </figure>
+    <div style="flex-basis: 100%; height: 0;"></div>; <!-- this will force wrapping to the next line -->
 
-<div class="gallery">
-  <a href="/baie">
-  <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Bathroom.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9CYXRocm9vbS5qcGciLCJpYXQiOjE3MDg2NDA4MjQsImV4cCI6MzE1NTI3NzEwNDgyNH0.ZmMUes9N0uyNGpCGpt6KfSYF6fwYYUXMt9g2xjPfUmU&t=2024-02-22T22%3A27%3A05.407Z"></a>
-  <div class="desc">Baie</div>
-  
-</div>
-
-<div class="gallery">
-  <a href="/accesorii-bucatarie">
-  <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Kitchen.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9LaXRjaGVuLmpwZyIsImlhdCI6MTcwODY0MDg0MiwiZXhwIjozMTU1Mjc3MTA0ODQyfQ.mxk4Cy5k2GHA7J6rP46Ludm0bbo4qBWKyOQlN-J4rbI&t=2024-02-22T22%3A27%3A22.974Z">
-  </a> 
- <div class="desc">Bucatarie</div>
-  
-</div>
-
-
+  </div>
 <?php include 'footer.php'; ?>
 </body>
 </html>
