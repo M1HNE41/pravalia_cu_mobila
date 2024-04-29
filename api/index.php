58f396ad9e9bbc87cbcb3ed435f8a02b67b7b3ec
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-29 08:59:37 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -266,7 +266,7 @@ body {
 <div id="mz-gallery-container">
 
   <div id="mz-gallery" class="flex flex-wrap justify-between mx-auto space-x-4" style="max-width: 1200px;">
-    <figure style ="width: 30%; margin-bottom: 20px">
+    <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Dormitor.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9Eb3JtaXRvci5qcGciLCJpYXQiOjE3MDg2NDA3NzYsImV4cCI6MzE1NTI3NzEwNDc3Nn0.LQao52pUQp4-CPqNf0qt7SsLgQ1zZo8EgIzO0-uMKxQ&t=2024-02-22T22%3A26%3A17.390Z" alt="Statue of Liberty" width="4" height="100">
       <figcaption>Statue of Liberty</figcaption>
       <div></div>
@@ -275,7 +275,7 @@ body {
       <div></div>
     </figure>
 
-    <figure style ="width: 30%; margin-bottom: 20px">
+    <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwODAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z" alt="Night Sky" width="100" height="700">
       <figcaption>Night Sky</figcaption>
       <div></div>
@@ -284,7 +284,7 @@ body {
       <div></div>
     </figure>
     <div style="flex-basis: 100%; height: 0;"></div> <!-- this will force wrapping to the next line -->
-    <figure style ="width: 30%; margin-bottom: 20px">
+    <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Dormitor.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9Eb3JtaXRvci5qcGciLCJpYXQiOjE3MDg2NDA3NzYsImV4cCI6MzE1NTI3NzEwNDc3Nn0.LQao52pUQp4-CPqNf0qt7SsLgQ1zZo8EgIzO0-uMKxQ&t=2024-02-22T22%3A26%3A17.390Z" alt="Ravine Between Rocks" width="400" height="700">
       <figcaption>Ravine Between Rocks</figcaption>
       <div></div>
@@ -293,7 +293,7 @@ body {
       <div></div>
     </figure>
 
-    <figure style ="width: 30%; margin-bottom: 20px">
+    <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwODAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z" alt="Ravine Between Rocks" width="400" height="700">
       <figcaption>Ravine Between Rocks</figcaption>
       <div></div>
