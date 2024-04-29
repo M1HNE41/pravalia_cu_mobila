8f6216c61dcf48be7b823fc11f0f0da0c9a596e1
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-29 11:54:08 +0300

    Update struct.php

--- api/struct.php
+++ api/struct.php
@@ -155,7 +155,7 @@ body, header {
  
  
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<meta name="google-site-verification" content="cegB27eo8bVivjtxdVsHOLTnA-91EPyeYNmgvg-BhWw" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.0.1/tailwind.min.css'>
 
