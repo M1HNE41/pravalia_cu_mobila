7e6382a8a29a1a5ba1e54735217b42aaaebec4cc
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-29 21:40:39 +0300

    Update struct.php

--- api/struct.php
+++ api/struct.php
@@ -210,7 +210,7 @@ function ipToConcatenatedString($ip) {
             $username = pg_fetch_assoc($result_user)['username'];
             echo '<a href="#">Welcome, ' . $username . '</a>';
             echo '<form method="post">
-    <button type="submit" name="logout" class="bottom-line">Logout</button>
+     <a class="bottom-line" name="logout">Logout</a>
 </form>';
             if (isset($_POST['logout'])) {
                 $delete_query = "DELETE FROM sessions WHERE session_id = $sessionId";
