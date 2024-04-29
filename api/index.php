d56cf9bc2538025ac323a8476aefe6dd7edf00ad
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-29 15:08:24 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -301,12 +301,9 @@ body {
     </figure>
 </div>
 </body>
-    <div style="clear:both;"></div>
-  <div class="footer">
-    <div style="padding-right:35px">
-    <a class="fa fa-facebook" href="https://facebook.com" target="_blank"> Facebook </a>
-    <a class="fa fa-twitter" href="https://twitter.com" target="_blank"> Twitter </a>
-    <a class="fa fa-instagram" href="https://instagram.com" target="_blank"> Instragram </a>
+    <?php
+include 'footer.php';
+?>
     </div>
   </div>
 </html>
