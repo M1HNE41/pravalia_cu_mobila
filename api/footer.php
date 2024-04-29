bb8fd20940c5532bfc8b51fcef770093366976a3
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-29 14:59:31 +0300

    Update footer.php

--- api/footer.php
+++ api/footer.php
@@ -1,14 +1,207 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<style type="">
+		@import url("https://fonts.googleapis.com/css2?family=Noto+Serif+Display:wght@300&family=Rubik:wght@400;500;700&display=swap");
 
+:root {
+  --light: #ffffff;
+  --dark: #000;
+}
+body {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+h3 {
+  font-family: "Noto Serif Display", serif;
+  font-size: 2.3rem;
+  font-weight: 300;
+  text-align: center;
+}
+a {
+  text-decoration: none;
+  color: var(--dark);
+  font-weight: 400;
+  transition: 0.3s ease-in;
+  border-bottom: 1px solid transparent;
+  margin-bottom: 0.5rem;
+  display: inline-flex;
+}
+a:hover {
+  border-bottom: 1px solid var(--dark);
+}
+ul {
+  text-align: center;
+  list-style-type: none;
+  padding: 0;
+}
+button {
+  appearance: none;
+  border: 0;
+  background: transparent;
+}
+.flex {
+  display: flex;
+}
+
+.footer {
+  position: relative;
+  display: flex;
+  align-items: center;
+  min-height: 100vh;
+}
+
+
+
+
+
+@media (min-width: 675px) {
+  .layout {
+    display: flex;
+    flex-wrap: nowrap;
+    column-gap: 2rem;
+  }
+  .w-50 {
+    width: 50%;
+  }
+  .w-25 {
+    width: 25%;
+  }
+}
+form {
+  position: relative;
+}
+svg {
+  margin: 0.5rem;
+}
+.c-2 {
+  margin-top: 3.5rem;
+}
+.footer_copyright {
+  color: var(--light);
+}
+
+	</style>
+</head>
 <body>
-<div style="clear:both;"></div>
-  <div class="footer">
-    <div style="padding-right:35px">
-    <a class="fa fa-facebook" href="https://facebook.com" target="_blank"> Facebook </a>
-    <a class="fa fa-twitter" href="https://twitter.com" target="_blank"> Twitter </a>
-    <a class="fa fa-instagram" href="https://instagram.com" target="_blank"> Instragram </a>
+<footer class="footer">
+
+  <div class="container">
+    <div class="footer_inner">
+      <div class="c-footer">
+        <div class="layout">
+          <div class="layout_item w-50">
+            <div class="newsletter">
+              <h3 class="newsletter_title">Pravalia cu Mobila: Transforma-ti spatiul într-un univers de rafinament</h3>
+            </div>
+          </div>
+
+          <div class="layout_item w-25">
+            <nav class="c-nav-tool">
+              <h4 class="c-nav-tool_title" style="text-align: center;">Menu</h4>
+              <ul class="c-nav-tool_list">
+                <li>
+                  <a href="/collections/all" class="c-link">Shop All</a>
+                </li>
+
+                <li>
+                  <a href="/pages/about-us" class="c-link">About Us</a>
+                </li>
+
+                <li>
+                  <a href="/blogs/community" class="c-link">Community</a>
+                </li>
+                <li>
+                  <a href="#" class="c-link">Vibes</a>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
+
+          <div class="layout_item w-25">
+            <nav class="c-nav-tool">
+              <h4 class="c-nav-tool_title" style="text-align: center;">Support</h4>
+              <ul class="c-nav-tool_list">
+
+                <li class="c-nav-tool_item">
+                  <a href="/pages/shipping-returns" class="c-link">Shipping &amp; Returns</a>
+                </li>
+
+                <li class="c-nav-tool_item">
+                  <a href="/pages/help" class="c-link">Help &amp; FAQ</a>
+                </li>
+
+                <li class="c-nav-tool_item">
+                  <a href="/pages/terms-conditions" class="c-link">Terms &amp; Conditions</a>
+                </li>
+
+                <li class="c-nav-tool_item">
+                  <a href="/pages/privacy-policy" class="c-link">Privacy Policy</a>
+                </li>
+
+                <li class="c-nav-tool_item">
+                  <a href="/pages/contact" class="c-link">Contact</a>
+                </li>
+
+                <li class="c-nav-tool_item">
+                  <a href="/account/login" class="c-link">
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
+        </div>
+        <div class="layout c-2">
+          <div class="layout_item w-50">
+            <ul class="flex">   
+            </ul>
+          </div>
+          <div class="layout_item w-25">
+            <ul class="flex">
+              <li>
+                <a href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z" />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z" />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M12 2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2zm0 5a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm6.5-.25a1.25 1.25 0 0 0-2.5 0 1.25 1.25 0 0 0 2.5 0zM12 9a3 3 0 1 1 0 6 3 3 0 0 1 0-6z" />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="layout_item w-25" style="display:flex;justify-content: end;align-items: center;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm1 10h3l-4-4-4 4h3v4h2v-4z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer_copyright">
+      <p>&copy; 2024 Petcu Mihnea Inc.</p>
     </div>
   </div>
+</footer>
 </body>
 </html>
