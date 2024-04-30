f37b745aa2a881fe7c909c4f5a9b0bfbef4b2996
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-04-30 10:58:09 +0300

    Update struct.php

--- api/struct.php
+++ api/struct.php
@@ -28,7 +28,7 @@ body, header {
             display: flex;
             align-items: center;
             justify-content: space-between;
-            padding: 20px;
+            padding: 40px;
         }
 
         .logo {
