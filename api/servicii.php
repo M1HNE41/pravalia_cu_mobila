135aa7b042bc8ef9bfc475da990df3c01d0d7baf
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-05-06 10:11:15 +0300

    Update servicii.php

--- api/servicii.php
+++ api/servicii.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="ro">
-<?php include 'servicii-css.php';?>
+<?php include 'style/servicii-css.php';?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
