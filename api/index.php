50a695acb7f90f8a506baaf987bd7a76a1373a15
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-05-06 19:12:33 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -16,37 +16,21 @@ include 'struct.php';
     <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Dormitor.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9Eb3JtaXRvci5qcGciLCJpYXQiOjE3MDg2NDA3NzYsImV4cCI6MzE1NTI3NzEwNDc3Nn0.LQao52pUQp4-CPqNf0qt7SsLgQ1zZo8EgIzO0-uMKxQ&t=2024-02-22T22%3A26%3A17.390Z" alt="Dormitor" onclick="window.location.href='/mobila-dormitor';" ontouchstart="window.location.href='/mobila-dormitor';">
       <figcaption>Dormitor</figcaption>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
     </figure>
 
     <figure style ="margin-bottom: 20px">
-      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwODAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z" alt="Living" onclick="window.location.href='/mobila-living';" ontouchstart="window.location.href='/mobila-living';">
+      <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Living.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9MaXZpbmcuanBnIiwiaWF0IjoxNzA4NjQwO<!© 2024 Petcu Mihnea – –>DAzLCJleHAiOjMxNTUyNzcxMDQ4MDN9.MoNYrDS6s84nJ6J-CLK8Lla8wCFjTRvt6W1fCl2qoDA&t=2024-02-22T22%3A26%3A44.500Z" alt="Living" onclick="window.location.href='/mobila-living';" ontouchstart="window.location.href='/mobila-living';">
       <figcaption>Living</figcaption>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
     </figure>
     <div style="flex-basis: 100%; height: 0;"></div> <!-- this will force wrapping to the next line -->
     <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Bathroom.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9CYXRocm9vbS5qcGciLCJpYXQiOjE3MTQzODkwNDksImV4cCI6MzE1NTMxNDM4OTA0OX0.PDAlA62GVGp_fUL25o0LNNiUTczBswDkwnCYGx3s1As&t=2024-04-29T11%3A10%3A50.200Z" alt="Bathroom"onclick="window.location.href='/baie';" ontouchstart="window.location.href='/baie';">
       <figcaption>Baie</figcaption>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
     </figure>
 
     <figure style ="margin-bottom: 20px">
       <img src="https://piasuguypoushrpezbmu.supabase.co/storage/v1/object/sign/products/index/Kitchen.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9pbmRleC9LaXRjaGVuLmpwZyIsImlhdCI6MTcxNDM4OTEwMywiZXhwIjozMTU1MzE0Mzg5MTAzfQ.yOFwgL7uVFRL81PEktc2b2cKBCZ6NtHvb0lygXOyKYY&t=2024-04-29T11%3A11%3A44.053Z" alt="Kitchen" onclick="window.location.href='/accesorii-bucatarie';" ontouchstart="window.location.href='/accesorii-bucatarie';">
       <figcaption>Bucatarie</figcaption>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
     </figure>
 </div>
 </body>
